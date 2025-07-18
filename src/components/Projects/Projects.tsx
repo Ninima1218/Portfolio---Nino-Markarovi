74f2567d916f7d7f9beb03d0839212da68c636ca
Ninima1218 <ninima1218@gmail.com>
2025-07-18 14:39:00 +0400

    add new projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,21 +1,5 @@
-import Justice from '@assets/projects/justice.png';
-import LanguageApp from '@assets/projects/language-app.png';
 import './Projects.css';
-
-const projects = [
-  {
-    title: 'Responsive Website for a Law Firm',
-    description: 'Elegant and functional site showcasing services and contact information. Built with HTML, CSS, and JS for seamless cross-device experience.',
-    imageUrl: Justice,
-    link: 'https://project-itgirlschool.github.io/fr83_justice/#services',
-  },
-  {
-    title: 'Speak Up: Language Learning Platform',
-    description: 'Self-directed template using React + MobX. Features grammar, vocabulary themes, and listening components for immersive learning.',
-    imageUrl: LanguageApp,
-    link: 'https://github.com/Ninima1218/language-app/tree/mobx',
-  },
-];
+import { projects } from '../projectsData/projectsData';
 
 const Projects = () => (
   <section id="projects" className="projects-section">
@@ -26,9 +10,18 @@ const Projects = () => (
           <img src={imageUrl} alt={title} />
           <h3>{title}</h3>
           <p>{description}</p>
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            VIEW PROJECT
-          </a>
+          {stack && (
+            <ul className="project-stack">
+              {stack.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+          )}
+          {link && (
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              VIEW PROJECT
+            </a>
+          )}
         </div>
       ))}
     </div>
